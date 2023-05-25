7b0af32ee1fc36aca6025e3d7aad05e819c3b316
kavinddd <fnvin@hotmail.com>
2023-05-25 15:26:24 +0700

    Edited README.md + Added main.py and requirements.txt

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+from google.cloud import bigquery
+
+class Config:
+    dataset_id = os.environ.get('dataset_id')
+    table_name = os.environ.get('table_name')
+    url = os.environ.get('url')
+
+def get_data_api(url) -> dict:
+
+    response = requests.get(url)
+    result = response.json()
+
+    return result
+
+def insert_data(event, context):
+
+    client = bigquery.Client()
+    dataset_ref = client.dataset(Config.dataset_id)
+
+    raw = get_data(Config.url)
+    record = [(
+        raw['time']['updatedISO'],
+        raw['bpi']['THB']['rate_float'],
+        raw['bpi']['USD']['rate_float']
+    )]
+
+    table_ref = dataset_ref.table(Config.table_name)
+    table = client.get_table(table_ref)
+    result = client.insert_rows(table, record)
+    return result
